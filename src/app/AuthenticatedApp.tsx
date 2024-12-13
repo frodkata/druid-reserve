0992ad8281a67a1187a5376b02b981616aac35aa
frodkata <frodkata@gmail.com>
2024-12-13 17:07:02 +0200

    cssbaseline

--- src/app/AuthenticatedApp.tsx
+++ src/app/AuthenticatedApp.tsx
@@ -1,12 +1,9 @@
 import Home from "./screens/Home";
-import BottomNavBar from "../components/UI/BottomNavBar";
 import { Routes, Route } from "react-router";
-import { CssBaseline } from "@mui/material";
 
 const AuthenticatedApp = () => {
 	return (
 		<>
-			<CssBaseline />
 			<Routes>
 				<Route path="/home" element={<Home />} />
 			</Routes>
